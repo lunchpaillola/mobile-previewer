227cb8f22855ec3b14d2c05ee87b2dca173f7598
Lola-Ojabowale <lola.ojabowale@gmail.com>
2023-08-21 21:47:27 -0400

    initial commit

--- libraries.js
+++ libraries.js
@@ -1,5 +1,7 @@
 import * as lib1 from '@protonapp/material-components'
+import * as lib2 from 'video-chat-by-daily-early-access'
 
 export default {
   '@protonapp/material-components': lib1,
+  'video-chat-by-daily-early-access': lib2,
 }
